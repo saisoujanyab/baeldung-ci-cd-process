59cf8caf5fdeeb46e72fcfc37eee68f722894bfb
Frank Nkuyahaga <franknkuyahaga@Franks-MacBook-Pro.local>
2020-06-30 10:07:15 +0300

    Initializing application

---
+++ src/main/java/com/hagafrank/baeldung/BaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.hagafrank.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaeldungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hagafrank/baeldung/BaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hagafrank.baeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeldungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
